4c253eb180d1863a612d9f5b1c4d60946e7e4c87
NaqeutZ06 <moinjamie@gmail.com>
2025-12-15 03:01:34 +0100

    Add AspectRatio component for fixed media ratios

---
+++ frontend/src/components/ui/AspectRatio.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    data-slot="aspect-ratio"
+    class="relative w-full"
+    :style="{ paddingTop: paddingTop }"
+  >
+    <div class="absolute inset-0">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  /** Verhältnis Breite:Höhe, z.B. 16/9 oder 1 */
+  ratio?: number
+}>()
+
+const paddingTop = computed(() => {
+  const r = props.ratio && props.ratio > 0 ? props.ratio : 1
+  return `${100 / r}%`
+})
+</script>
