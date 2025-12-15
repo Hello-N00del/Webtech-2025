1bf3804219c93a8bba0336882a62abdb1557c2b3
NaqeutZ06 <moinjamie@gmail.com>
2025-12-15 04:06:34 +0100

    Add Separator component (radix-vue port)

---
+++ frontend/src/components/ui/Separator.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Separator as RadixSeparator } from "radix-vue"
+import { cn } from "./utils"
+
+const props = withDefaults(
+  defineProps<{
+    class?: string
+    orientation?: "horizontal" | "vertical"
+    decorative?: boolean
+  }>(),
+  {
+    orientation: "horizontal",
+    decorative: true,
+  },
+)
+</script>
+
+<template>
+  <RadixSeparator
+    v-bind="$attrs"
+    :orientation="props.orientation"
+    :decorative="props.decorative"
+    data-slot="separator-root"
+    :class="
+      cn(
+        'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
+        props.class,
+      )
+    "
+  />
+</template>
